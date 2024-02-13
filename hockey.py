6fb70592030da362e89bfb6be8b4fe9fe6d04605
MaxB3435 <bortasmax@gmail.com>
2024-02-13 10:28:06 +0100

    Update hockey.py

--- hockey.py
+++ hockey.py
@@ -1,19 +1,31 @@
 import random
 
 streak = 0
+streaklength = 8
 match = 0
-misschance = 0.8
-hit = 0
+misschance = 0.5
 numstreak = 0
+season = 0
+numseason = 10000
 
-while (match < 50):
-    match = match + 1
-    hit = random.random()
-    if (hit < misschance):
-        streak = streak + 1
-        print (streak)
-    else:
-        streak = 0
-    if (streak > 7):
-        numstreak = numstreak + 1
+while season <= numseason:
+    while match <= 52:
+        if season == numseason:
+            print(numstreak/(numseason/100),'%')
+            exit()
+        if match == 52:
+           match = 0
+           season = season +1
+           
+        match = match + 1
+        if random.random() < misschance:
+            streak = streak + 1
+        else:
+            streak = 0
+        if streak == streaklength:
+            numstreak = numstreak + 1
+            streak = 0
+            match = 0
+            season = season +1
+
 
