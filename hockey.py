86eb7379a5dd622671b3e39d459b8772bed7ed07
MaxB3435 <bortasmax@gmail.com>
2024-02-13 09:22:12 +0100

    Update hockey.py

--- hockey.py
+++ hockey.py
@@ -1,14 +1,17 @@
+import random
+
 streak = 0
 match = 0
-misschance = 0.5
+misschance = 0.8
 hit = 0
 numstreak = 0
 
-while (match < 10):
+while (match < 50):
     match = match + 1
-    hit = random(0,1)
+    hit = random.random()
     if (hit < misschance):
         streak = streak + 1
+        print (streak)
     else:
         streak = 0
     if (streak > 7):
